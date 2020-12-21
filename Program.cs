8b78e3cfa70bb3390c08641d10df032f70739704
Brian Hart <brian.hart@xylogix.net>
2020-12-21 14:57:09 -0500

    Add handler for Application.ThreadException event

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
 using System.Windows.Forms;
 using EmployeeManager.BusinessLayer;
 using EmployeeManager.Properties;
@@ -16,6 +18,11 @@ namespace EmployeeManager
         [STAThread]
         public static void Main()
         {
+            Application.SetUnhandledExceptionMode(
+                UnhandledExceptionMode.CatchException
+            );
+            Application.ThreadException += OnThreadException;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
@@ -27,6 +34,39 @@ namespace EmployeeManager
         }
 
         /// <summary>
+        ///     Logs the error info from the <paramref name="exception" /> object reference
+        ///     provided.
+        /// </summary>
+        /// <param name="exception">
+        ///     A <see cref="T:System.Exception" /> that contains the
+        ///     error information.
+        /// </param>
+        /// <remarks>
+        ///     This method does nothing if the <paramref name="exception" />
+        ///     parameter has a <c>null</c> value.
+        /// </remarks>
+        private static void CaptureErrorInfo(Exception exception)
+        {
+            if (exception == null) return;
+
+            var logPath = Path.Combine(
+                Path.GetTempPath(),
+                $"{Path.GetFileNameWithoutExtension(Application.ExecutablePath)}.log"
+            );
+            if (File.Exists(logPath))
+                File.Delete(logPath);
+
+            File.WriteAllLines(
+                logPath,
+                new[]
+                {
+                    $"Most recent connection string used: {EmployeesConnectionTester.LastUsedConnectionString}",
+                    $"Exception details: {exception.Message}\r\n\t{exception.StackTrace}"
+                }
+            );
+        }
+
+        /// <summary>
         ///     Checks to ensure the database is available, and then connects to it.
         /// </summary>
         private static void ConnectToDatabase()
@@ -34,6 +74,8 @@ namespace EmployeeManager
             // double check that the database is available.
             if (!EmployeesConnectionTester.IsDatabaseOnline())
             {
+                CaptureErrorInfo(EmployeesConnectionTester.LastError);
+
                 ProgressWindow.Instance.Hide();
 
                 MessageBox.Show(
@@ -44,6 +86,24 @@ namespace EmployeeManager
             }
 
             EmployeesServiceManager.Instance.InitializeAll();
+        }
+
+        /// <summary>
+        ///     Handles the
+        ///     <see cref="E:System.Windows.Forms.Application.ThreadException" /> event.
+        /// </summary>
+        /// <param name="sender">
+        ///     Reference to the instance of the object that raised this
+        ///     event.
+        /// </param>
+        /// <param name="e">
+        ///     A <see cref="T:System.Threading.ThreadExceptionEventArgs" />
+        ///     that contains the event data.
+        /// </param>
+        private static void OnThreadException(object sender,
+            ThreadExceptionEventArgs e)
+        {
+            CaptureErrorInfo(e.Exception);
         }
     }
 }
